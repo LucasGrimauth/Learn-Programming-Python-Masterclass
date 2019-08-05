608f2d7f8f91baa9ef97025025a86a158a6af636
Lucas Grimauth Evangelista <Lucas.Grimauth@ibm.com>
2019-08-05 19:38:09 -0300

    blackjack

--- ModulesFunctions/functions.py
+++ ModulesFunctions/functions.py
@@ -2,11 +2,11 @@ def python_food():
   print("pandas and dogs")
 
 
-def center_text(*args, sep=" ", end="\n", file=None, flush=False):
+def center_text(*args, sep=" "):
   text = "" 
   for arg in args:
     text += str(arg) + sep
   left_margin = (70 - len(text)) // 2
-  print(" " * left_margin, text, end=end, file=file, flush=flush)
+  return " " * left_margin + text
 
-center_text("first", "second", 3, 4, "pandas")
+print(center_text("first", "second", 3, 4, "pandas"))
